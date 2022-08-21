7df32ad12fa51fb14220a172825345fb4e277d46
Dalton Messmer <messmerd97@gmail.com>
2022-08-21 18:14:59 -0400

    v0.1.1 release

--- include/config.h
+++ include/config.h
@@ -1,5 +1,5 @@
 // The configured options and settings for dmf2mod
 #define DMF2MOD_VERSION_MAJOR 0
 #define DMF2MOD_VERSION_MINOR 1
-#define DMF2MOD_VERSION_PATCH 0
-#define DMF2MOD_VERSION "0.1.0"
+#define DMF2MOD_VERSION_PATCH 1
+#define DMF2MOD_VERSION "0.1.1"
